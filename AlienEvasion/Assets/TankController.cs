4fac5e6b4ae7f5a420f4447dcf381c691a005a38
Chandresh Pant <chandresh.pant@gmail.com>
2023-06-17 20:43:43 +0530

    Tank controller script

--- AlienEvasion/Assets/TankController.cs
+++ AlienEvasion/Assets/TankController.cs
@@ -5,18 +5,41 @@ using UnityEngine;
 public class TankController : MonoBehaviour
 {
 
+    // Variable to reference the Rigidbody of the tank
     Rigidbody2D tankRb;
 
-    // Start is called before the first frame update
+    // Rigidbodies of the three tires and the body
+    [SerializeField]
+    Rigidbody2D tire1Rb, tire2Rb, tire3Rb;
+
+    [SerializeField]
+    float tankSpeed = 20;
+
+    private float tankMovement;
+
     void Start()
     {
+        // This code makes sure the center of mass of the tank is slightly on left
+        // This makes sure that the tank does not fall down in the front because of long barrel
         tankRb = GetComponent<Rigidbody2D>();
         tankRb.centerOfMass -= new Vector2(1, 0);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Get the value of Horizontal movement
+        // Minus it as we want the tires to rotate opposite of it
+        tankMovement = -Input.GetAxis("Horizontal");
+    }
 
+    private void FixedUpdate()
+    {
+        // Add Torque to all the tires and car body
+        float tankTorque = tankMovement * tankSpeed * Time.fixedDeltaTime;
+        tire1Rb.AddTorque(tankTorque);
+        tire2Rb.AddTorque(tankTorque);
+        tire3Rb.AddTorque(tankTorque);
+        tankRb.AddTorque(tankTorque);
     }
 }
