d29a85925399b57715c370f3a1a1bebdfffef3cc
Chandresh Pant <chandresh.pant@gmail.com>
2023-06-17 20:12:13 +0530

    Tank and Terrain - Basic versions

---
+++ AlienEvasion/Assets/TankController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+
+    Rigidbody2D tankRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tankRb = GetComponent<Rigidbody2D>();
+        tankRb.centerOfMass -= new Vector2(1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
