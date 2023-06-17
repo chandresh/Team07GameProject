37dcee18ec5729f28aef9824e7c76ce8c5a1b80e
Chandresh Pant <chandresh.pant@gmail.com>
2023-06-17 20:52:27 +0530

    Camera follow the tank

---
+++ AlienEvasion/Assets/FollowTheTank.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTheTank : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform tankTransform;
+
+    private Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Store the original difference between camera and tank positions
+        cameraOffset = transform.position - tankTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the camera position so that the camera follows the tank
+        transform.position = tankTransform.position + cameraOffset;
+    }
+}
