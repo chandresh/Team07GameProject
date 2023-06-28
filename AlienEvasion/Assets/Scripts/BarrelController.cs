525b7958b643cdd9103b6fe2e5e76b0072920691
Chandresh Pant <chandresh.pant@gmail.com>
2023-06-28 12:20:09 +0530

    Prototype03 - Basic fire done

--- AlienEvasion/Assets/Scripts/BarrelController.cs
+++ AlienEvasion/Assets/Scripts/BarrelController.cs
@@ -19,6 +19,18 @@ public class BarrelController : MonoBehaviour
     // Offsets for the 'up' position
     [SerializeField] private Vector3 upBarrelPositionOffset = new Vector3(-0.3f, 0.8f, 0);
 
+    // Get the shell prefab from the inspector
+    [SerializeField] private GameObject shellPrefab;
+
+    // Shell Barrel position offset
+    [SerializeField] private Vector3 shellBarrelPositionOffset = new Vector3(1.2f, 0, 0);
+
+    // A timer for the delay between shots
+    private float fireDelay = 0.5f;
+    private float nextFireTime = 0;
+
+
+
     private void Start()
     {
         // Store the initial angle of the barrel as the default
@@ -46,6 +58,36 @@ public class BarrelController : MonoBehaviour
             // If the player is pressing down, rotate the barrel down
             RotateDown();
         }
+
+        // If the player is holding the Fire1 button (space by default), fire the shell
+        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireDelay;
+            Fire(upAngle);
+        }
+
+    }
+
+    void Fire(float angle)
+    {
+        // Fire the shell
+
+        // Instantiate the shell at the barrel's position plus some offset
+        GameObject shell = Instantiate(shellPrefab, transform.position + shellBarrelPositionOffset, Quaternion.identity);
+
+        // Calculate the direction to apply force to the shell
+        Vector2 shellDirection = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+
+        // Get the shell's Rigidbody2D component
+        Rigidbody2D shellRb = shell.GetComponent<Rigidbody2D>();
+
+        // Add impulse force to the shell in the direction calculated above
+        shellRb.AddForce(shellDirection, ForceMode2D.Impulse);
+
+        // Set the shell's velocity and fire it
+        // float shellSpeed = 20f;
+        // shellRb.velocity = shellRb.transform.right * shellSpeed;
+
     }
 
     void RotateUp()
---
+++ AlienEvasion/Assets/Scripts/ShellController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellController : MonoBehaviour
+{
+
+    // Destroy the shell when it leaves the screen
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // Destroy the shell when it hits something
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Destroy the shell if other object's tag is Ground
+        if (other.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+            Debug.Log("Shell hit " + other.gameObject.name);
+        }
+    }
+}
