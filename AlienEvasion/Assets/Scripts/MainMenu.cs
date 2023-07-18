4f25b1bc66f5159fe2876e3c73bffe0b76d4b6a0
Nathan Norton <nathangnorton@gmail.com>
2023-07-18 23:34:31 +0200

    play button added to main menu

---
+++ AlienEvasion/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }    
+}
