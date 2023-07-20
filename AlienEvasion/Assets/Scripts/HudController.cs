28c46ce4dc51cce41d6484219086e6f2b17baae5
Nathan Norton <nathangnorton@gmail.com>
2023-07-20 16:13:51 +0200

    currency display

---
+++ AlienEvasion/Assets/Scripts/HudController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HudController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerStatsController playerStats;
+
+    [SerializeField]
+    private TMP_Text currencyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateCurrency();        
+    }
+
+    void updateCurrency()
+    {
+        // update text value with the current currency value in player stats
+        currencyText.text = $"Currency: {playerStats.currentCurrency.ToString()}";
+    }
+}
---
+++ AlienEvasion/Assets/Scripts/PlayerStatsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatsController : MonoBehaviour
+{
+    public int currentCurrency;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCurrency = 0;
+    }
+
+    public void increaseCurrency(int amount)
+    {
+        currentCurrency += amount;
+    }
+}
