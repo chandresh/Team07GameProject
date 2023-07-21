f330079cae94c893b9a11627e8042d75c5c664c0
Nathan Norton <nathangnorton@gmail.com>
2023-07-21 23:24:03 +0200

    aliens do damage to player instead of destroy

--- AlienEvasion/Assets/Scripts/AlienBombController.cs
+++ AlienEvasion/Assets/Scripts/AlienBombController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AlienBombController : MonoBehaviour
 {
-
     // Destroy the bomb when it leaves the screen
     private void OnBecameInvisible()
     {
@@ -19,8 +18,14 @@ public class AlienBombController : MonoBehaviour
         if (other.gameObject.CompareTag("PlayerParts"))
         {
             // Find and destroy the object with tag "Player"
-            GameObject player = GameObject.FindWithTag("Player");
-            Destroy(player);
+            //GameObject player = GameObject.FindWithTag("Player");
+            //Destroy(player);
+
+            // gets the player stats controller and calls change health
+            // reducing the players health by 10
+            GameObject playerController = GameObject.Find("PlayerStatsController");
+            PlayerStatsController controller = (PlayerStatsController)playerController.GetComponent(typeof(PlayerStatsController));
+            controller.changeHealth(-10);
         }
     }
 }
