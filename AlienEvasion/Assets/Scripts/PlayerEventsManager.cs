c43babc55c41a1754e93a66e7affe781e5921de6
Chandresh Pant <chandresh.pant@gmail.com>
2023-08-08 05:49:12 +0530

    Added PlayerEventsManager

---
+++ AlienEvasion/Assets/Scripts/PlayerEventsManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerEventsManager : MonoBehaviour
+{
+    public static event Action OnPlayerGotHit;
+    public static event Action<Vector3> OnPlayerLoseLife;
+
+    public static void PlayerGotHit()
+    {
+        OnPlayerGotHit?.Invoke();
+    }
+
+    public static void PlayerLostLife(Vector3 position)
+    {
+        OnPlayerLoseLife?.Invoke(position);
+    }
+}
