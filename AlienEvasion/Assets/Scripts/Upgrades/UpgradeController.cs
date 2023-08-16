853d301462d0265416cdd1a4adfdc82f2eac945d
Nathan Norton <nathangnorton@gmail.com>
2023-08-16 20:51:22 +0200

    upgrade events manager added

---
+++ AlienEvasion/Assets/Scripts/Upgrades/UpgradeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeController : MonoBehaviour
+{
+    private int healthUpgradeLevel;
+    private int armourUpgradeLevel;
+    private int shieldUpgradeLevel;
+    private int shotSpeedUpgradeLevel;
+    private int turrentUpgradeLevel;
+    private int airstrikeUpgradeLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // all upgrade levels start at 0
+        healthUpgradeLevel = 0;
+        armourUpgradeLevel = 0;
+        shieldUpgradeLevel = 0;
+        shotSpeedUpgradeLevel = 0;
+        turrentUpgradeLevel = 0;
+        airstrikeUpgradeLevel = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
