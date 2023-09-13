e0b5ecebfed0e37614f5c727f55c32e2c44a9a5e
Chandresh Pant <chandresh.pant@gmail.com>
2023-09-13 19:06:51 +0530

    Add movement to enemies

--- AlienEvasion/Assets/EnemyMovement.cs
+++ AlienEvasion/Assets/EnemyMovement.cs
@@ -2,18 +2,25 @@
 
 public class EnemyMovement : MonoBehaviour
 {
-    float verticalSpeed = 1.0f;
-    float horizontalSpeed = 0.5f;
-    float verticalRange = 2.0f;
-    float horizontalRange = 1.0f;
+    float verticalSpeed = 0.0f;
+    float horizontalSpeed = 0.0f;
+    float verticalRange = 0.0f;
+    float horizontalRange = 0.0f;
+
+    [SerializeField] bool allowVerticalMovement = true;
     private Vector3 startPosition;
 
     void Start()
     {
         startPosition = transform.position;
-        verticalSpeed = (1 + GameManager.CurrentLevel) * 0.5f;
+
+        if (allowVerticalMovement)
+        {
+            verticalSpeed = (1 + GameManager.CurrentLevel) * 0.5f;
+            verticalRange = 1 + GameManager.CurrentLevel;
+        }
+
         horizontalSpeed = (1 + GameManager.CurrentLevel) * 0.25f;
-        verticalRange = 1 + GameManager.CurrentLevel;
         horizontalRange = (1 + GameManager.CurrentLevel) * 0.5f;
     }
 
