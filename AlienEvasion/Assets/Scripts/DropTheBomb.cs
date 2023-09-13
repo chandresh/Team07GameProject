4ced0d9136a99a3dc88f261ed1022f7e7c68919c
Chandresh Pant <chandresh.pant@gmail.com>
2023-09-13 15:57:17 +0530

    Alien UFO bomb issue fixed
    - Fixed based on Playtesters feedback
    - Noe the bombs are always fired
    - Upto 3 bombs are fired so the game is challanging

--- AlienEvasion/Assets/Scripts/DropTheBomb.cs
+++ AlienEvasion/Assets/Scripts/DropTheBomb.cs
@@ -25,15 +25,19 @@ public class DropTheBomb : MonoBehaviour
     void DropBomb()
     {
         // Instantiate the alienBombPrefab at dropPosition with a random rotation / angle
-        // Only if there is no alienBombPrefab present
-        // Find by tag
+        // Only if there is up to 3 alienBombPrefab present
 
-        if (GameObject.FindWithTag("AlienBomb") == null)
+        GameObject[] existingBombs = GameObject.FindGameObjectsWithTag("AlienBomb");
+
+        if (existingBombs.Length < 3)
         {
             GameObject alienBomb = Instantiate(alienBombPrefab, dropPosition.position, Quaternion.Euler(0, 0, Random.Range(0, 360)));
             alienBomb.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
             am.BindSoundToGameObjectLocation(name: "ufo-shoot", gameObject: alienBomb);
             am.Play("ufo-shoot");
+
+            // Destroy the alienBombPrefab after 1 second
+            Destroy(alienBomb, 2f);
         }
 
     }
