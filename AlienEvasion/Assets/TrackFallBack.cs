e72b0819774800505eaa01673d0595e3e1abe95e
Chandresh Pant <chandresh.pant@gmail.com>
2023-09-13 20:11:54 +0530

    Playtester issue fixed: Game over if tank falls off the hill

---
+++ AlienEvasion/Assets/TrackFallBack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackFallBack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        // If player touches the falloff trigger object - player dies
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("PlayerParts"))
+        {
+            Invoke("PlayerDied", 2.0f);
+        }
+    }
+
+
+    void PlayerDied()
+    {
+        GameManager.PlayerDied();
+    }
+
+
+
+}
